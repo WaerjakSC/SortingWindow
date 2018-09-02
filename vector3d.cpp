70a4d3c642c42915a46ac684dabf43151378aad4
Jack Kalvoey <waerjaksc@gmail.com>
2018-09-02 03:35:01 +0200

    Mostly done with gui, getting started on vector3d class and sorting function tomorrow

---
+++ vector3d.cpp
@@ -0,0 +1,6 @@
+#include "vector3d.h"
+
+vector3d::vector3d()
+{
+
+}
---
+++ vector3d.h
@@ -0,0 +1,11 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+
+class vector3d
+{
+public:
+    vector3d();
+};
+
+#endif // VECTOR3D_H
